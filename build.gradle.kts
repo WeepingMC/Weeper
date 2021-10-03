a01737964fa27e2962858fc2596d65b2fdaeb6ac
Yannick Lamprecht <yannicklamprecht@live.de>
2021-10-03 13:44:58 +0200

    switch back to paper as upstream

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
+import io.papermc.paperweight.util.constants.PAPERCLIP_CONFIG
+
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "7.0.0" apply false
-    id("io.papermc.paperweight.patcher") version "1.1.11"
+    id("io.papermc.paperweight.patcher") version "1.1.12"
     `maven-publish`
 }
 
@@ -9,17 +11,18 @@ repositories {
     mavenCentral()
     maven("https://papermc.io/repo/repository/maven-public/") {
         content {
-            onlyForConfigurations("paperclip")
+            onlyForConfigurations(PAPERCLIP_CONFIG)
         }
     }
 }
 
 dependencies {
-    remapper("org.quiltmc:tiny-remapper:0.4.3:fat")
+    remapper("net.fabricmc:tiny-remapper:0.6.0:fat")
+    decompiler("net.minecraftforge:forgeflower:1.5.498.12")
     paperclip("io.papermc:paperclip:2.0.1")
 }
 
-subprojects {
+allprojects {
     apply(plugin = "java")
     apply(plugin = "maven-publish")
 
@@ -28,10 +31,19 @@ subprojects {
             languageVersion.set(JavaLanguageVersion.of(16))
         }
     }
+}
+
+subprojects {
 
     tasks.withType<JavaCompile>().configureEach {
-        options.encoding = "UTF-8"
+        options.encoding = Charsets.UTF_8.name()
         options.release.set(16)
+    }
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
     }
 
     repositories {
@@ -42,20 +54,36 @@ subprojects {
         maven("https://repo.aikar.co/content/groups/aikar")
         maven("https://repo.md-5.net/content/repositories/releases/")
         maven("https://hub.spigotmc.org/nexus/content/groups/public/")
-        maven("https://jitpack.io")
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    }
+
+    apply<MavenPublishPlugin>()
+    publishing {
+        repositories {
+            maven {
+                authentication {
+                    credentials(PasswordCredentials::class)
+                }
+                url = uri("https://nexus.endrealm.net/repository/toothpick/")
+                name = "endrealm"
+            }
+            maven {
+                authentication {
+                    credentials(PasswordCredentials::class)
+                }
+                url = uri("https://dev.craftstuebchen.de/repo/repository/snapshots/")
+                name = "cs"
+            }
+        }
     }
 }
 
 paperweight {
     serverProject.set(project(":Weeper-Server"))
+    remapRepo.set("https://maven.fabricmc.net/")
+    decompileRepo.set("https://files.minecraftforge.net/maven/")
 
-    useStandardUpstream("purpur") {
-        url.set(github("pl3xgaming", "Purpur"))
-        ref.set(providers.gradleProperty("upstreamRef"))
-
-        withStandardPatcher {
-            baseName("Purpur")
+    usePaperUpstream(providers.gradleProperty("upstreamRef")) {
+        withPaperPatcher {
 
             apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
             apiOutputDir.set(layout.projectDirectory.dir("Weeper-API"))
@@ -79,27 +107,4 @@ tasks.register("cleanup"){
 tasks.paperclipJar {
     destinationDirectory.set(rootProject.layout.projectDirectory)
     archiveFileName.set("weeper-paperclip.jar")
-}
-
-
-subprojects {
-    apply<MavenPublishPlugin>()
-    publishing {
-        repositories {
-            maven {
-                authentication {
-                    credentials(PasswordCredentials::class)
-                }
-                url = uri("https://nexus.endrealm.net/repository/toothpick/")
-                name = "endrealm"
-            }
-            maven {
-                authentication {
-                    credentials(PasswordCredentials::class)
-                }
-                url = uri("https://dev.craftstuebchen.de/repo/repository/snapshots/")
-                name = "cs"
-            }
-        }
-    }
 }
