247aee2f9f59aae3e6b0e410eaa22f9ac7ed79e3
Yannick Lamprecht <yannicklamprecht@live.de>
2021-11-18 23:35:03 +0100

    clean repositories, move settings up

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import io.papermc.paperweight.util.constants.PAPERCLIP_CONFIG
 
 plugins {
     java
-    id("com.github.johnrengelman.shadow") version "7.0.0" apply false
+    id("com.github.johnrengelman.shadow") version "7.1.0" apply false
     id("io.papermc.paperweight.patcher") version "1.2.0"
     `maven-publish`
 }
@@ -48,12 +48,7 @@ subprojects {
 
     repositories {
         mavenCentral()
-        maven("https://oss.sonatype.org/content/groups/public/")
         maven("https://papermc.io/repo/repository/maven-public/")
-        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
-        maven("https://repo.aikar.co/content/groups/aikar")
-        maven("https://repo.md-5.net/content/repositories/releases/")
-        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
     }
 
     apply<MavenPublishPlugin>()
@@ -80,14 +75,13 @@ subprojects {
 paperweight {
     serverProject.set(project(":Weeper-Server"))
 
+    remapRepo.set("https://maven.fabricmc.net/")
+    decompileRepo.set("https://files.minecraftforge.net/maven/")
+
     usePaperUpstream(providers.gradleProperty("upstreamRef")) {
         withPaperPatcher {
-
             apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
             apiOutputDir.set(layout.projectDirectory.dir("Weeper-API"))
-
-            remapRepo.set("https://maven.fabricmc.net/")
-            decompileRepo.set("https://files.minecraftforge.net/maven/")
 
             serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
             serverOutputDir.set(layout.projectDirectory.dir("Weeper-Server"))
