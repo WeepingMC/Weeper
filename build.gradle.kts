cf1e252557aaac503be87f04fdbc1d062f2b1296
Yannick Lamprecht <yannicklamprecht@live.de>
2022-05-04 21:07:03 +0200

    Update upstream

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("io.papermc.paperweight.patcher") version "1.3.5"
+    id("io.papermc.paperweight.patcher") version "1.3.6"
 }
 
 allprojects {
@@ -71,8 +71,8 @@ repositories {
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.1:fat")
-    decompiler("net.minecraftforge:forgeflower:1.5.498.22")
+    remapper("net.fabricmc:tiny-remapper:0.8.2:fat")
+    decompiler("net.minecraftforge:forgeflower:1.5.498.29")
     paperclip("io.papermc:paperclip:3.0.2")
 }
 
