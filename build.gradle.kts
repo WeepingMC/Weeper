40d72004389cccbd9e89b428fed583a4e80d7cb0
Yannick Lamprecht <yannicklamprecht@live.de>
2023-02-13 15:53:24 +0100

    Update paperweight version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("io.papermc.paperweight.patcher") version "1.4.1"
+    id("io.papermc.paperweight.patcher") version "1.5.0"
 }
 
 val paperMavenPublicUrl = "https://papermc.io/repo/repository/maven-public/"
