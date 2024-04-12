471e259d531ee16db2a68dbea431b5d8018777fe
Yannick Lamprecht <yannicklamprecht@live.de>
2024-04-12 23:24:01 +0200

    update upstream, paperweight and follow upstream vineflower adaption

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "8.1.1-SNAPSHOT"
-    id("io.papermc.paperweight.patcher") version "1.5.13"
+    id("io.papermc.paperweight.patcher") version "1.5.15"
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
@@ -52,7 +52,7 @@ repositories {
 
 dependencies {
     remapper("net.fabricmc:tiny-remapper:0.10.1:fat")
-    decompiler("net.minecraftforge:forgeflower:2.0.627.2")
+    decompiler("org.vineflower:vineflower:1.11.0-20240412.144930-14")
     paperclip("io.papermc:paperclip:3.0.3")
 }
 
