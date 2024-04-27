6ec1592a0ac06dc8d377541d2b59120645aaa723
Yannick Lamprecht <yannicklamprecht@live.de>
2024-04-27 23:11:03 +0200

    rebase api patches

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "8.1.1-SNAPSHOT"
-    id("io.papermc.paperweight.patcher") version "1.6.0-SNAPSHOT"
+    id("io.papermc.paperweight.patcher") version "1.6.0"
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
