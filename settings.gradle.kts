3b55b2cf5466354357e2bcaefc9871b4195167d6
Yannick Lamprecht <yannicklamprecht@live.de>
2024-04-28 13:35:03 +0200

    add foojay convention resolver

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 rootProject.name = "Weeper"
 
 for (name in listOf("weeper-api", "weeper-server", "weeper-mojangapi")) {
