27cd1198d7d2f9e44b36e30e5cb29daceda01e69
Yannick Lamprecht <yannicklamprecht@live.de>
2025-04-23 00:35:36 +0200

    add javadoc

--- weeper-api/src/main/java/com/github/weepingmc/packet/options/Animation.java
+++ weeper-api/src/main/java/com/github/weepingmc/packet/options/Animation.java
@@ -1,27 +1,58 @@
 package com.github.weepingmc.packet.options;
 
+/**
+ * Represents various types of animations that can be used in the application.
+ */
 public enum Animation {
 
     /**
-     * use {@link #SWING_MAIN_HAND}
+     * Deprecated. Use {@link #SWING_MAIN_HAND} instead.
      */
     @Deprecated
     SWING_MAIN_ARM,
-    SWING_MAIN_HAND,
+
     /**
-     * use {@link #CRITICAL_HIT}
+     * Represents the animation for swinging the main hand.
+     */
+    SWING_MAIN_HAND,
+
+    /**
+     * Deprecated. Use {@link #CRITICAL_HIT} instead.
+     *
+     * @deprecated Scheduled for removal in version 1.22.0.
+     * @since 1.21.3
      */
     @Deprecated(forRemoval = true, since = "1.21.3")
     @org.jetbrains.annotations.ApiStatus.ScheduledForRemoval(inVersion = "1.22.0")
     TAKE_DAMAGE,
-    LEAVE_BED,
-    SWING_OFFHAND,
+
     /**
-     * use {@link #CRITICAL_HIT}
+     * Represents the animation for leaving a bed.
+     */
+    LEAVE_BED,
+
+    /**
+     * Represents the animation for swinging the offhand.
+     */
+    SWING_OFFHAND,
+
+    /**
+     * Deprecated. Use {@link #CRITICAL_HIT} instead.
+     *
+     * @deprecated Scheduled for removal in version 1.22.0.
+     * @since 1.21.3
      */
     @Deprecated(forRemoval = true, since = "1.21.3")
     @org.jetbrains.annotations.ApiStatus.ScheduledForRemoval(inVersion = "1.22.0")
     CRITICAL_EFFECT,
+
+    /**
+     * Represents the animation for a critical hit.
+     */
     CRITICAL_HIT,
+
+    /**
+     * Represents the animation for a magic critical effect.
+     */
     MAGIC_CRITICAL_EFFECT
 }
