b2d87a5822dd972918ebb1d1e5ef47b86104ddb0
Yannick Lamprecht <yannicklamprecht@live.de>
2026-01-03 20:58:51 +0100

    allow listing underlying custom goal

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -2,6 +2,10 @@ package io.papermc.testplugin;
 
 
 import com.destroystokyo.paper.SkinParts;
+import com.destroystokyo.paper.entity.ai.Goal;
+import com.destroystokyo.paper.entity.ai.GoalKey;
+import com.destroystokyo.paper.entity.ai.GoalType;
+import com.destroystokyo.paper.entity.ai.MobGoals;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
@@ -16,10 +20,13 @@ import io.papermc.paper.command.brigadier.argument.CustomArgumentType;
 import io.papermc.paper.datacomponent.item.ResolvableProfile;
 import io.papermc.paper.disguise.DisguiseData;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import java.util.EnumSet;
 import java.util.Locale;
 import java.util.concurrent.CompletableFuture;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Mannequin;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Pose;
@@ -51,6 +58,8 @@ public final class TestPlugin extends JavaPlugin implements Listener {
                                         );
                                         zombie.setCustomNameVisible(true);
                                         zombie.customName(Component.text("ysl", NamedTextColor.RED));
+
+                                        Bukkit.getMobGoals().addGoal(zombie, 1, new TestGoal(zombie));
                                     });
                                     player.sendMessage(Component.text("Test command executed!", NamedTextColor.GREEN));
                                 }
@@ -99,6 +108,37 @@ public final class TestPlugin extends JavaPlugin implements Listener {
             }
 
             return builder.buildFuture();
+        }
+    }
+
+    private static class TestGoal implements Goal<Zombie> {
+
+        private final Zombie zombie;
+
+        private TestGoal(Zombie zombie) {
+            this.zombie = zombie;
+        }
+
+
+        @Override
+        public boolean shouldActivate() {
+            return true;
+        }
+
+        @Override
+        public void tick() {
+            zombie.getNearbyEntities(10, 10, 10).stream().filter(Player.class::isInstance)
+                .forEach(player -> player.sendMessage(Component.text("Hello, I am a zombie!", NamedTextColor.RED)));
+        }
+
+        @Override
+        public GoalKey<Zombie> getKey() {
+            return GoalKey.of(Zombie.class, new NamespacedKey("test-plugin", "test-goal"));
+        }
+
+        @Override
+        public EnumSet<GoalType> getTypes() {
+            return EnumSet.of(GoalType.MOVE);
         }
     }
 
