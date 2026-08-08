c3293e9db31c22050262365828be7051e82771d1
Yannick Lamprecht <yannicklamprecht@live.de>
2026-08-08 22:51:09 +0200

    moar jd

--- weeper-api/src/main/java/com/github/weepingmc/packet/PacketPipeline.java
+++ weeper-api/src/main/java/com/github/weepingmc/packet/PacketPipeline.java
@@ -6,15 +6,28 @@ import org.bukkit.scoreboard.Team;
 import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
 
+/**
+ * Some api handling packets.
+ */
 @NullMarked
 public interface PacketPipeline {
 
+    /**
+     * Creates a new packet step builder.
+     *
+     * @return a new instance
+     */
     PacketStepBuilder create();
 
+    /**
+     * Creates a entity metadata builder.
+     *
+     * @return a new instance
+     */
     EntityMetaBuilder createEntityMetaBuilder();
 
     /**
-     * Creates a skin part builder
+     * Creates a skin part builder.
      *
      * @return skinpart builder
      * @deprecated Use {@link org.bukkit.Server#newSkinPartsBuilder()}
